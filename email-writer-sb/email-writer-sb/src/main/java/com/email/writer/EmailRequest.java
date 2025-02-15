cc5eda03d0d5e28c3f9017284383159dcf30d559
ru2003 <ruchitanchaudhari145@gmail.com>
2025-02-15 18:06:19 +0530

    Initial Commit

--- email-writer-sb/email-writer-sb/src/main/java/com/email/writer/EmailRequest.java
+++ email-writer-sb/email-writer-sb/src/main/java/com/email/writer/EmailRequest.java
@@ -1,4 +1,16 @@
 package com.email.writer;
 
+
+import lombok.Data;
+
+@Data
 public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+    public CharSequence getTone() {
+    }
+
+    public char[] getEmailContent() {
+    }
 }
