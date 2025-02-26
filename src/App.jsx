9cb8965690782d709d352c3166a0e966802dd7f8
ru2003 <ruchitanchaudhari145@gmail.com>
2025-02-26 16:51:45 +0530

    Updated frontend with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
 
 import { useState } from 'react'
 import './App.css'
+import { TextField } from '@mui/material';
 
 function App() {
-
-  const[emailContent , setEmailContent]  = useState('');
+   /* emailContent =a state variable that stores the content of the email
+   setEmailContent =a function to update the emailContent
+   useState('') = Initilizes emailContent with an empty string('') */
+  const [emailContent , setEmailContent]  = useState('');
   const [tone, setTone] = useState('');
   const [generatedReply, setGeneratedReply] = useState('');
-  const[loading, setLoading ] = useState(false);
-  const[error, setError] = useState('');
+  const [loading, setLoading ] = useState(false);
+  const [error, setError] = useState('');
+  
+  
   
 
+  return ( 
+    <Container maxWidth="md" sx={{py:4}}>
+      <Typography variant='h3' component="h1" gutterBottom>
+        Email Reply Generator
+      </Typography>
+
+      <Box sx={{mx:3}}>
+        <TextField
+        fullWidthmultiline
+        rows={6}
+        variant='outlined'
+        label="Original Email Content"
+        value={emailContent || ' '}
+        onChange={}
+        />
 
 
+      </Box>
+    </Container>
+                                                                    
+   
 
-  return (
-    <>
-     <p>hello</p>
-    </>
   )
 }
 
