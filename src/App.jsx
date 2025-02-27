7a318882f4f59331a6333d0e72e36ba308aa621a
ru2003 <ruchitanchaudhari145@gmail.com>
2025-02-28 01:25:32 +0530

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,35 @@
-
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
-import { TextField } from '@mui/material';
+import { Container, Typography, Box, TextField } from '@mui/material';
+
 
 function App() {
-   /* emailContent =a state variable that stores the content of the email
-   setEmailContent =a function to update the emailContent
-   useState('') = Initilizes emailContent with an empty string('') */
-  const [emailContent , setEmailContent]  = useState('');
-  const [tone, setTone] = useState('');
-  const [generatedReply, setGeneratedReply] = useState('');
-  const [loading, setLoading ] = useState(false);
-  const [error, setError] = useState('');
-  
-  
-  
+  const [emailContent , setEmailContent] = useState('');
+  const [tone , setTone] = useState('');
+  const [ generatedReply , setGeneratedReply] = useState('');
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState(''); 
 
-  return ( 
+  return (
     <Container maxWidth="md" sx={{py:4}}>
       <Typography variant='h3' component="h1" gutterBottom>
         Email Reply Generator
       </Typography>
-
       <Box sx={{mx:3}}>
         <TextField
-        fullWidthmultiline
+        fullWidth
+        multiline
         rows={6}
-        variant='outlined'
         label="Original Email Content"
-        value={emailContent || ' '}
-        onChange={}
+        value={emailContent || ''}
+        onChange={(e) => setEmailContent(e.target.value)}
+        
         />
 
-
+        
       </Box>
     </Container>
-                                                                    
-   
+
 
   )
 }
