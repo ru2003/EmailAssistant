1385b5b9482d017604bc08e321f2ca0a50eced53
ru2003 <ruchitanchaudhari145@gmail.com>
2025-03-03 23:50:34 +0530

    new changes

--- emai-writer-extension/content.js
+++ emai-writer-extension/content.js
@@ -1,8 +1,62 @@
 console.log("Email Writer Extension - Content Script Loaded");
 
-// Define the injectButton function if it's not already defined
+
+function createAIButton() {
+    const button = document.createElement('div');
+    button.className = 'T-I J-J5-Ji aoO v7 T-I-atl L3';
+    button.style.marginRight = '8px';
+    button.innerHTML = 'AI Reply';
+    button.setAttribute('data-tooltip' , 'Generate AI Reply');
+    return button;
+}
+
+
+function findComposeToolbar() {
+    const selectors = [
+        '.btC',
+        '.IZ',
+        '[role="toolbar',
+        '.gU.Up'
+    ];
+    for (const selector of selectors) {
+        const toolbar = document.querySelector(selector);
+        if (toolbar) {
+            return toolbar;
+        }
+        return null;
+    }
+}
+
+
+
+
 function injectButton() {
-    
+    const existingButton = document.querySelector('.ai-reply-button');
+    if (existingButton) existingButton.remove();
+
+
+    const toolbar = findComposeToolbar();
+    if (!toolbar) {
+        console.log("Toolbar not found");
+        return;
+    }
+
+    console.log("Toolbar found , creating AI button");
+    const button = createAIButton();
+    button.classList.add('ai-reply-button');
+
+    button.addEventListener('click' , async () => {
+           try{
+            button.innerHTML = 'Generating...';
+            button.disabled = true;
+
+            const emailContent = getEmailContent();
+           }
+
+
+    });
+
+    toolbar.insertBefore(button, toolbar.firstChild);
 }
 
 
